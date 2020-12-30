493072490b66456aa0b9940a26180a2843028af9
unknown <dyomin.dyominalexander2011@yandex.ru>
2020-12-30 12:05:38 +0300

    Updated README.md

--- src/selection_controller.ts
+++ src/selection_controller.ts
@@ -9,8 +9,17 @@ export class SelectionController {
 			new TextCommandData('lrwts.cancelSelection', this.cancelSelection.bind(this)),
 			new TextCommandData('lrwts.setSelectionMark', this.setSelectionMark.bind(this)),
 			new TextCommandData('lrwts.selectBetweenQuotes', this.selectBetweenQuotes.bind(this)),
-			new TextCommandData('lrwts.addSelectionToNextFindMatch', this.addSelectionToNextFindMatch.bind(this))
+			new TextCommandData('lrwts.addSelectionToNextFindMatch', this.addSelectionToNextFindMatch.bind(this)),
+			new TextCommandData('lrwts.setMultilineCursor', this.setMultilineCursors.bind(this))
 		])
+	}
+
+	async setMultilineCursors() {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			editor.selections.push(new vscode.Selection(new vscode.Position(0, 0), new vscode.Position(0, 1)));
+			editor.selections.push(new vscode.Selection(new vscode.Position(1, 0), new vscode.Position(1, 1)));
+		}
 	}
 
 	async selectBetweenQuotes() {
